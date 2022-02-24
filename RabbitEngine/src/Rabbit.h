394a4ec2b83fbec37fdd20be8115a7209a8e0d88
Zheap <aphrodite_sudan@outlook.com>
2022-02-24 19:40:11 +0800

    Setup basic Application and Entry point

---
+++ RabbitEngine/src/Rabbit.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// For use by Rabbit applications
+
+#include "Rabbit/Application.h"
+
+// -------------Entry Point---------------
+#include "Rabbit/EntryPoint.h"
+// ---------------------------------------

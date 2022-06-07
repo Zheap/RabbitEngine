62be48d3d4dbeb566066a1056dedd27d2b9edb82
Zheap <aphrodite_sudan@outlook.com>
2022-06-07 11:11:25 +0800

    Added transform for shader renderer

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,11 +1,14 @@
 #include <Rabbit.h>
 
 #include "imgui/imgui.h"
+
+#include <glm/gtc/matrix_transform.hpp>
+
 class ExampleLayer : public Rabbit::Layer
 {
 public:
     ExampleLayer()
-        : Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), m_CameraPosition(0.0f)
+        : Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), m_CameraPosition(0.0f), m_SquarePosition(0.0f)
     {
         m_VertexArray.reset(Rabbit::VertexArray::Create());
 
@@ -33,10 +36,10 @@ public:
 
 
         float squareVertices[3 * 4] = {
-            -0.75f, -0.75f, 0.0f,
-             0.75f, -0.75f, 0.0f,
-             0.75f,  0.75f, 0.0f,
-            -0.75f,  0.75f, 0.0f
+            -0.5f, -0.5f, 0.0f,
+             0.5f, -0.5f, 0.0f,
+             0.5f,  0.5f, 0.0f,
+            -0.5f,  0.5f, 0.0f
         };
         m_SquareVA.reset(Rabbit::VertexArray::Create());
         std::shared_ptr<Rabbit::VertexBuffer> squareVB;
@@ -60,6 +63,7 @@ public:
             layout(location = 1) in vec4 a_Color;
 
             uniform mat4 u_ViewProjection;
+            uniform mat4 u_Transform;
 
             out vec3 v_Position;
             out vec4 v_Color;
@@ -68,7 +72,7 @@ public:
             {
                 v_Position = a_Position;
                 v_Color = a_Color;
-                gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+                gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
             }
         )";
 
@@ -95,13 +99,14 @@ public:
             layout(location = 0) in vec3 a_Position;
 
             uniform mat4 u_ViewProjection;
+            uniform mat4 u_Transform;
 
             out vec3 v_Position;
 
             void main()
             {
                 v_Position = a_Position;
-                gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+                gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
             }
         )";
 
@@ -130,15 +135,25 @@ public:
         else if (Rabbit::Input::IsKeyPressed(RB_KEY_RIGHT))
             m_CameraPosition.x += m_CameraMoveSpeed * ts;
 
-        if (Rabbit::Input::IsKeyPressed(RB_KEY_DOWN))
-            m_CameraPosition.y -= m_CameraMoveSpeed * ts;
-        else if (Rabbit::Input::IsKeyPressed(RB_KEY_UP))
+        if (Rabbit::Input::IsKeyPressed(RB_KEY_UP))
             m_CameraPosition.y += m_CameraMoveSpeed * ts;
+        else if (Rabbit::Input::IsKeyPressed(RB_KEY_DOWN))
+            m_CameraPosition.y -= m_CameraMoveSpeed * ts;
 
         if (Rabbit::Input::IsKeyPressed(RB_KEY_A))
             m_CameraRotation += m_CameraRotationSpeed * ts;
         if (Rabbit::Input::IsKeyPressed(RB_KEY_D))
             m_CameraRotation -= m_CameraRotationSpeed * ts;
+
+        //if (Rabbit::Input::IsKeyPressed(RB_KEY_J))
+        //    m_SquarePosition.x -= m_SquareMoveSpeed * ts;
+        //else if (Rabbit::Input::IsKeyPressed(RB_KEY_L))
+        //    m_SquarePosition.x += m_SquareMoveSpeed * ts;
+
+        //if (Rabbit::Input::IsKeyPressed(RB_KEY_I))
+        //    m_SquarePosition.y += m_SquareMoveSpeed * ts;
+        //else if (Rabbit::Input::IsKeyPressed(RB_KEY_K))
+        //    m_SquarePosition.y -= m_SquareMoveSpeed * ts;
 
 
         Rabbit::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1.0f });
@@ -149,7 +164,18 @@ public:
 
         Rabbit::Renderer::BeginScene(m_Camera);
 
-        Rabbit::Renderer::Submit(m_BlueShader, m_SquareVA);
+        glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+        for (int y = 0; y < 20; y++)
+        {
+            for (int x = 0; x < 20; x++)
+            {
+                glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+                glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+                Rabbit::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+            }
+        }
+
         Rabbit::Renderer::Submit(m_Shader, m_VertexArray);
 
         Rabbit::Renderer::EndScene();
@@ -198,6 +224,9 @@ private:
 
     float m_CameraRotation = 0.0f;
     float m_CameraRotationSpeed = 180.0f;
+
+    glm::vec3 m_SquarePosition;
+    float m_SquareMoveSpeed = 5.0f;
 };
 
 class Sandbox : public Rabbit::Application

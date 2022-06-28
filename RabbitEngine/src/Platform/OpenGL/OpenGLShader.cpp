8b70745cfa36c28a38ee5acfb06d897f44c9c3d4
Zheap <aphrodite_sudan@outlook.com>
2022-06-28 11:01:31 +0800

    Renderer2D API inprovements

--- RabbitEngine/src/Platform/OpenGL/OpenGLShader.cpp
+++ RabbitEngine/src/Platform/OpenGL/OpenGLShader.cpp
@@ -72,6 +72,13 @@ namespace Rabbit {
         UploadUniformInt(name, value);
     }
 
+    void OpenGLShader::SetFloat(const std::string& name, float value)
+    {
+        RB_PROFILE_FUNCTION();
+
+        UploadUniformFloat(name, value);
+    }
+
     void OpenGLShader::SetFloat3(const std::string& name, const glm::vec3& value)
     {
         RB_PROFILE_FUNCTION();

1a2e67364d9e2a7025ad8b2d316496b42d1e6451
Zheap <aphrodite_sudan@outlook.com>
2022-07-19 12:49:31 +0800

    Remove project's generated file

--- RabbitEngine/src/Rabbit/Core/EntryPoint.h
+++ RabbitEngine/src/Rabbit/Core/EntryPoint.h
@@ -8,17 +8,17 @@ int main(int argc, char** argv)
 {
     Rabbit::Log::Init();
 
-    RB_PROFILE_BEGIN_SESSION("Startup", "RabbitProfile-Startup.json");
+    //RB_PROFILE_BEGIN_SESSION("Startup", "RabbitProfile-Startup.json");
     auto app = Rabbit::CreateApplication();
-    RB_PROFILE_END_SESSION();
+    //RB_PROFILE_END_SESSION();
 
-    RB_PROFILE_BEGIN_SESSION("Runtime", "RabbitProfile-Runtime.json");
+    //RB_PROFILE_BEGIN_SESSION("Runtime", "RabbitProfile-Runtime.json");
     app->Run();
-    RB_PROFILE_END_SESSION();
+    //RB_PROFILE_END_SESSION();
 
-    RB_PROFILE_BEGIN_SESSION("ShutDown", "RabbitProfile-ShutDown.json");
+    //RB_PROFILE_BEGIN_SESSION("ShutDown", "RabbitProfile-ShutDown.json");
     delete app;
-    RB_PROFILE_END_SESSION();
+    //RB_PROFILE_END_SESSION();
 }
 
 #endif

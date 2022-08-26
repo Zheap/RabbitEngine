34b6b54186af77259b29d43e8fa34d348818392d
Zheap <aphrodite_sudan@outlook.com>
2022-08-26 14:39:05 +0800

    Content Browser panel thumbnail layout

--- Rabbitnut/src/Panels/ContentBrowserPanel.h
+++ Rabbitnut/src/Panels/ContentBrowserPanel.h
@@ -2,6 +2,8 @@
 
 #include <filesystem>
 
+#include "Rabbit/Renderer/Texture.h"
+
 namespace Rabbit {
 
     class ContentBrowserPanel
@@ -12,6 +14,9 @@ namespace Rabbit {
         void OnImGuiRender();
     private:
         std::filesystem::path m_CurrentDirectory;
+
+        Ref<Texture2D> m_DirectoryIcon;
+        Ref<Texture2D> m_FileIcon;
     };
 
 }

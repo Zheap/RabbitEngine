70453f48f3ed52036ff0267ae80d309af503d67d
Zheap <aphrodite_sudan@outlook.com>
2022-09-05 11:42:51 +0800

    Added loging glm vectors, matrix and quaternions

--- RabbitEngine/src/Rabbit/Core/Log.h
+++ RabbitEngine/src/Rabbit/Core/Log.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include "Core.h"
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include "glm/gtx/string_cast.hpp"
+
 #include "spdlog/spdlog.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
 #include "spdlog/fmt/ostr.h"
@@ -20,6 +24,24 @@ namespace Rabbit {
         static Ref<spdlog::logger> s_ClientLogger;
     };
 
+}
+
+template<typename OStream, glm::length_t L, typename T, glm::qualifier Q>
+inline OStream& operator<<(OStream& os, const glm::vec<L, T, Q>& vector)
+{
+    return os << glm::to_string(vector);
+}
+
+template<typename OStream, glm::length_t C, glm::length_t R, typename T, glm::qualifier Q>
+inline OStream& operator<<(OStream& os, const glm::mat<C, R, T, Q>& matrix)
+{
+    return os << glm::to_string(matrix);
+}
+
+template<typename OStream, typename T, glm::qualifier Q>
+inline OStream& operator<<(OStream& os, glm::qua<T, Q> quaternion)
+{
+    return os << glm::to_string(quaternion);
 }
 
 // Core log macros

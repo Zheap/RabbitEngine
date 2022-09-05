58d3ed174c2edbc0ec286ae451f803554f67044e
Zheap <aphrodite_sudan@outlook.com>
2022-09-05 11:58:32 +0800

    Sync fix bugs

--- RabbitEngine/src/Platform/OpenGL/OpenGLTexture.cpp
+++ RabbitEngine/src/Platform/OpenGL/OpenGLTexture.cpp
@@ -17,7 +17,7 @@ namespace Rabbit {
 
         RB_CORE_ASSERT(m_InternalFormat & m_DataFormat, "Format not supported!")
 
-        glCreateTextures(GL_TEXTURE_2D, 1, &m_RendererID);
+            glCreateTextures(GL_TEXTURE_2D, 1, &m_RendererID);
         glTextureStorage2D(m_RendererID, 1, m_InternalFormat, m_Width, m_Height);  // this GL_RGB8 means how opengl is going to store our texture
 
         glTextureParameteri(m_RendererID, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
@@ -37,42 +37,46 @@ namespace Rabbit {
         stbi_uc* data = nullptr;
         {
             RB_PROFILE_SCOPE("stbi_load - OpenGLTexture2D::OpenGLTexture2D(const std::string&)");
-            data = stbi_load(path.c_str(), &width, &height, &channels, 0); 
+            data = stbi_load(path.c_str(), &width, &height, &channels, 0);
         }
-        RB_CORE_ASSERT(data, "Failed to load image!");
 
-        m_Width = width;
-        m_Height = height;
-
-        GLenum internalFormat = 0, dataFormat = 0;
-        if (channels == 4)
+        if (data)
         {
-            internalFormat = GL_RGBA8;
-            dataFormat = GL_RGBA;
+            m_IsLoaded = true;
+
+            m_Width = width;
+            m_Height = height;
+
+            GLenum internalFormat = 0, dataFormat = 0;
+            if (channels == 4)
+            {
+                internalFormat = GL_RGBA8;
+                dataFormat = GL_RGBA;
+            }
+            else if (channels == 3)
+            {
+                internalFormat = GL_RGB8;
+                dataFormat = GL_RGB;
+            }
+
+            m_InternalFormat = internalFormat;
+            m_DataFormat = dataFormat;
+
+            RB_CORE_ASSERT(m_InternalFormat & m_DataFormat, "Format not supported!")
+
+                glCreateTextures(GL_TEXTURE_2D, 1, &m_RendererID);
+            glTextureStorage2D(m_RendererID, 1, m_InternalFormat, m_Width, m_Height);  // this GL_RGB8 means how opengl is going to store our texture
+
+            glTextureParameteri(m_RendererID, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+            glTextureParameteri(m_RendererID, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+            glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_S, GL_REPEAT);
+            glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+            glTextureSubImage2D(m_RendererID, 0, 0, 0, m_Width, m_Height, m_DataFormat, GL_UNSIGNED_BYTE, data);  // send data to GPU, this GL_RGB means out data format
+
+            stbi_image_free(data);  // free data from cpu
         }
-        else if (channels == 3)
-        {
-            internalFormat = GL_RGB8;
-            dataFormat = GL_RGB;
-        }
-
-        m_InternalFormat = internalFormat;
-        m_DataFormat = dataFormat;
-
-        RB_CORE_ASSERT(m_InternalFormat & m_DataFormat, "Format not supported!")
-
-        glCreateTextures(GL_TEXTURE_2D, 1, &m_RendererID);
-        glTextureStorage2D(m_RendererID, 1, m_InternalFormat, m_Width, m_Height);  // this GL_RGB8 means how opengl is going to store our texture
-
-        glTextureParameteri(m_RendererID, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTextureParameteri(m_RendererID, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-        glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-        glTextureSubImage2D(m_RendererID, 0, 0, 0, m_Width, m_Height, m_DataFormat, GL_UNSIGNED_BYTE, data);  // send data to GPU, this GL_RGB means out data format
-
-        stbi_image_free(data);  // free data from cpu
     }
 
     OpenGLTexture2D::~OpenGLTexture2D()
@@ -88,7 +92,7 @@ namespace Rabbit {
 
         uint32_t bpc = m_DataFormat == GL_RGBA ? 4 : 3;  // bytes per channel
         RB_CORE_ASSERT(size == m_Width * m_Height * bpc, "Data must be entire texture!")
-        glTextureSubImage2D(m_RendererID, 0, 0, 0, m_Width, m_Height, m_DataFormat, GL_UNSIGNED_BYTE, data);  // send data to GPU, this GL_RGB means out data format
+            glTextureSubImage2D(m_RendererID, 0, 0, 0, m_Width, m_Height, m_DataFormat, GL_UNSIGNED_BYTE, data);  // send data to GPU, this GL_RGB means out data format
     }
 
     void OpenGLTexture2D::Bind(uint32_t slot) const
--- Rabbitnut/src/EditorLayer.cpp
+++ Rabbitnut/src/EditorLayer.cpp
@@ -475,12 +475,20 @@ namespace Rabbit {
 
     void EditorLayer::OpenScene(const std::filesystem::path& path)
     {
-        m_ActiveScene = CreateRef<Scene>();
-        m_ActiveScene->OnViewportResize((uint32_t)m_ViewportSize.x, (uint32_t)m_ViewportSize.y);
-        m_SceneHierarchyPanel.SetContext(m_ActiveScene);
+        if (path.extension().string() != ".rabbit")
+        {
+            RB_WARN("Could not load {0} - not a scene file", path.filename().string());
+            return;
+        }
 
+        Ref<Scene> newScene = CreateRef<Scene>();
         SceneSerializer serializer(m_ActiveScene);
-        serializer.Deserialize(path.string());
+        if (serializer.Deserialize(path.string()))
+        {
+            m_ActiveScene = newScene;
+            m_ActiveScene->OnViewportResize((uint32_t)m_ViewportSize.x, (uint32_t)m_ViewportSize.y);
+            m_SceneHierarchyPanel.SetContext(m_ActiveScene);
+        }
     }
 
     void EditorLayer::SaveSceneAs()

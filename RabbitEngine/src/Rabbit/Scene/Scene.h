4fef41d68950db334783ffd07a956d721484c69f
Zheap <aphrodite_sudan@outlook.com>
2022-09-18 11:54:29 +0800

    Added physics collider visualization

--- RabbitEngine/src/Rabbit/Scene/Scene.h
+++ RabbitEngine/src/Rabbit/Scene/Scene.h
@@ -36,6 +36,12 @@ namespace Rabbit {
 
         Entity GetPrimaryCameraEntity();
 
+        template<typename... Components>
+        auto GetAllEntitiesWith()
+        {
+            return m_Registry.view<Components...>();
+        }
+
     private:
         template<typename T>
         void OnComponentAdded(Entity entity, T& component);
